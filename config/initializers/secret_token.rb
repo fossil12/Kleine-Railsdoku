fcb2fb317ee0427cf6eecf5f7b5b7ffc2315e7b0
David Keller <dev@davidkeller.me>
2011-04-10 20:40:26 +0200

    Neue Railsinstanz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KleineRailsdoku::Application.config.secret_token = '0b5447c4072ff97aae0c6d94d18849d20c3c503044a95e95cc5c2d76869f88d9a3e2f6d42b0ed37caa3aaf294e65ebc39f618246040ddc2ab25d1c06c5691d57'
